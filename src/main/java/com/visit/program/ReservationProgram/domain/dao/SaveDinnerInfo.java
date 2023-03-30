0f95481289bc04deb3f69173c4347f7ab87d2cac
rapigen <hnko@rapigen-inc.com>
2023-03-30 19:22:04 +0900

    0330 commit

---
+++ src/main/java/com/visit/program/ReservationProgram/domain/dao/SaveDinnerInfo.java
@@ -0,0 +1,18 @@
+package com.visit.program.ReservationProgram.domain.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaveDinnerInfo {
+    private Long employee_id;
+    private Long dinner_reservation_id;
+    private Boolean is_checked;
+
+
+}
